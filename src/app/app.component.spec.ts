f1c33a696a298d13d060a0b2c0786d48dedb989f
Akshay <akshay.mhatre8989@gmail.com>
2020-07-16 22:29:31 +0530

    Universal Heading widget- Unit test cases fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { UniversalHeadingComponent } from './universal-heading/universal-heading.component';
@@ -7,7 +8,8 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       declarations: [
         AppComponent,
