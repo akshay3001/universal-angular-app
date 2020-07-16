4a00cffc56abcdce2bdc50d57fa4e9046d474574
Akshay <akshay.mhatre8989@gmail.com>
2020-07-16 22:24:00 +0530

    Universal Heading widget- Selector name and Title is user customizable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { UniversalHeadingComponent } from './universal-heading/universal-heading
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
